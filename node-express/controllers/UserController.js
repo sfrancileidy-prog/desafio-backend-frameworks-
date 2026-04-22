8c02fccb49bbc80df4310bed73378eff10b60971
sfrancileidy-prog <sfrancileidy@gmail.com>
2026-04-22 17:16:14 -0300

    Create UserController.js

---
+++ node-express/controllers/UserController.js
@@ -0,0 +1,13 @@
+const UserModel = require('../models/UserModel');
+
+class UserController {
+  static createUser(req, res) {
+    const { name, email } = req.body;
+
+    const user = new UserModel(name, email);
+
+    return res.status(201).json(user);
+  }
+}
+
+module.exports = UserController;
